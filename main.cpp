2bddec6b05aacd5e938b412ce24f584595369e2c
nweeks <noe.weeks@hotmail.com>
2021-02-25 19:20:09 +0100

    pretraitement termine

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
+#include "pretraitement.hpp"
 using namespace std;
 
-int main() {
-	
+
+int main(int ac, char **av) {
+	if (ac != 2)
+	{
+		cout << "nom du fichier attendu" << endl;
+		return 0;
+	}
+	string fichierEntree = "";
+	for (int i(0); av[1][i]; ++i)
+		fichierEntree += av[1][i];
+	Probleme probleme(fichierEntree);
 	return 0;
 }
